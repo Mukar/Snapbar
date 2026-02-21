81ffa8cf256f5bc31c0a6c2807d026a835003050
Mukar <masonwolfrum97@gmail.com>
2026-02-20 21:38:51 -0600

    Add files via upload

--- snap_toolbar/snapping_toolbar.gd
+++ snap_toolbar/snapping_toolbar.gd
@@ -15,41 +15,41 @@ func _ready():
 	for i in popup_children:
 		if i.title == "Snap Settings":
 			snap_dialog = i
-			line_edits = i.find_children("*", "LineEdit", true, false)
+			line_edits = i.find_children("*", "Range", true, false)
 	
 	snap_dialog.confirmed.connect(update_toolbar_from_popup)
 
 func _on_input_translate_value_changed(value: float) -> void:
-	line_edits[0].set_text(str(value))
+	line_edits[0].set_value_no_signal(value)
 	snap_dialog.confirmed.emit()
 
 func _on_input_rotation_value_changed(value: float) -> void:
-	line_edits[1].set_text(str(value))
+	line_edits[1].set_value_no_signal(value)
 	snap_dialog.confirmed.emit()
 
 func _on_input_scale_value_changed(value: float) -> void:
-	line_edits[2].set_text(str(value))
+	line_edits[2].set_value_no_signal(value)
 	snap_dialog.confirmed.emit()
 
 ## Updates toolbar values into popup, as Godot resets snapping values per scene by default.
 func load_toolbar_values():
-	line_edits[0].set_text(str(%input_translate.value))
-	line_edits[1].set_text(str(%input_rotation.value))
-	line_edits[2].set_text(str(%input_scale.value))
+	line_edits[0].set_value_no_signal(%input_translate.value)
+	line_edits[1].set_value_no_signal(%input_rotation.value)
+	line_edits[2].set_value_no_signal(%input_scale.value)
 	snap_dialog.confirmed.emit()
 
 ## Updates toolbar values with changes made in popup, if changes are made there.
 func update_toolbar_from_popup():
-	%input_translate.set_value_no_signal(float(line_edits[0].text))
-	%input_rotation.set_value_no_signal(float(line_edits[1].text))
-	%input_scale.set_value_no_signal(float(line_edits[2].text))
+	%input_translate.set_value_no_signal(line_edits[0].value)
+	%input_rotation.set_value_no_signal(line_edits[1].value)
+	%input_scale.set_value_no_signal(line_edits[2].value)
 	save_settings()
 
 ## Loads snap settings from file for persistence across editor shutdown.
 func load_settings(translate_increment: float, rotate_increment: float, scale_increment: float):
-	%input_translate.set_value_no_signal(float(translate_increment))
-	%input_rotation.set_value_no_signal(float(rotate_increment))
-	%input_scale.set_value_no_signal(float(scale_increment))
+	%input_translate.set_value_no_signal(translate_increment)
+	%input_rotation.set_value_no_signal(rotate_increment)
+	%input_scale.set_value_no_signal(scale_increment)
 	snap_dialog.confirmed.emit()
 
 ## Saves snap settings to file for persistence across editor shutdown.
